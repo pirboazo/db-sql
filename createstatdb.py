9a0b9d3a6e6127e52252dcd67ee6efc3dbe7285c
pirboazo <pib@pirboazo.net>
2022-04-14 18:00:18 +0200

    Ajout creation statdb

---
+++ createstatdb.py
@@ -0,0 +1,18 @@
+#!/home/pboizot/src/DB-sql/bin/python
+
+# import du driver de connection sqlite3
+import sqlite3
+
+# Etablissement de la connection
+DBConnection = sqlite3.connect('chinook.db')
+
+if DBConnection :
+    print("Connection established ..........")
+
+cur = DBConnection.cursor()
+cur.execute('create table statdb (nomtable text not NULL unique, nbligne integer not null, nbattr integer )')
+cur.close()
+
+DBConnection.close()
+
+exit()
