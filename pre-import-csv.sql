8b33645f1db39b42a9d28da9869e5eed69bc5c04
pirboazo <pib@pirboazo.net>
2022-04-23 15:16:42 +0200

    Ajout filtre sqlite_%

--- pre-import-csv.sql
+++ pre-import-csv.sql
@@ -1,4 +1,4 @@
 #
 #  exemple de select pour générer l'ensemble des ordre d'import csv
 #
-select ".import --csv --skip 1 "||"'csv/"||name||"_data.csv'"||" "||name from sqlite_master where type="table";
+select ".import --csv --skip 1 "||"'csv/"||name||"_data.csv'"||" "||name from sqlite_master where name  NOT LIKE 'sqlite_%' and type="table";
